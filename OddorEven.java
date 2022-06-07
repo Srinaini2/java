8f12a7c4d3743715e80101a6e77b9a131229387b
Srinaini2 <107024288+Srinaini2@users.noreply.github.com>
2022-06-07 15:56:41 +0530

    Add files via upload

---
+++ OddorEven.java
@@ -0,0 +1,22 @@
+package week1.day4;
+
+public class OddorEven {
+	public void great()
+	{
+		for(int i=1;i<=50;i++)
+		{
+			if(i%2==1)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OddorEven obj=new OddorEven();
+		obj.great();
+
+	}
+
+}
