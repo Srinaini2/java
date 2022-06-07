73e9d1b0843c71e096ed2f1d1bad36c4f6acc836
Srinaini2 <107024288+Srinaini2@users.noreply.github.com>
2022-06-07 15:56:05 +0530

    Add files via upload

---
+++ FibonaccisSeries.java
@@ -0,0 +1,18 @@
+package week1.day4;
+
+public class FibonaccisSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=0;
+		int b=1;
+		for(int i=1;i<=10;i++)
+		{
+			int c=a+b;
+			System.out.println(a);
+			
+		}
+
+	}
+
+}
