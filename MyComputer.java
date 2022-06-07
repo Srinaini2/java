490397fbc5815809050c4fd9357d34c822e2af3d
Srinaini2 <107024288+Srinaini2@users.noreply.github.com>
2022-06-07 15:39:53 +0530

    Add files via upload

---
+++ MyComputer.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class MyComputer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Computer obj=new Computer();
+		String
+		computerName=obj.computerName;
+		System.out.println(obj.computerName);
+		
+		}
+
+}
