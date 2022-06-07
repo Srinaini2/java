2363e15fc0e6217d6011620865876ababd5647b1
Srinaini2 <107024288+Srinaini2@users.noreply.github.com>
2022-06-07 15:47:12 +0530

    Add files via upload

---
+++ TwoWheeler.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+public class TwoWheeler {
+	int noOfWheeler=2;
+	short noOfmirror=2;
+	long chassisNumber=123451;
+	boolean isPunctured=false;
+	String bickName="Rx";
+	double runningKM=2345.987;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TwoWheeler two=new TwoWheeler();
+		System.out.println("noOfWheeler"+two.noOfWheeler);
+		System.out.println("noOfmirror"+two.noOfmirror);
+		System.out.println("chassisNumber"+two.chassisNumber);
+		System.out.println("isPunctured"+two.isPunctured);
+		System.out.println("bickName"+two.bickName);
+		System.out.println("runningKM"+two.runningKM);
+
+	}
+
+}
