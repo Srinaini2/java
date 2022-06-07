68a8f0e0b3b777d965ad3dab7c7f44a98e0288b8
Srinaini2 <107024288+Srinaini2@users.noreply.github.com>
2022-06-07 15:58:55 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,16 @@
+package week1.day4;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		while(a>0)
+		{
+			System.out.println(a);
+			a--;
+		}
+
+	}
+
+}
