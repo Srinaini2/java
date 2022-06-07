f1e65054816a6cd8540f27dc1aaae15c55c43496
Srinaini2 <107024288+Srinaini2@users.noreply.github.com>
2022-06-07 15:40:52 +0530

    Add files via upload

---
+++ ArithmeticOperator.java
@@ -0,0 +1,29 @@
+package week1.day2;
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArithmeticOperator object=new
+		ArithmeticOperator();
+		object.add();
+		object.sub();
+	}
+	public void add()
+	{
+		int a=12;
+		int b=8;
+		int c=a+b;
+		System.out.println(c);
+	}
+	public void sub()
+	{
+		int a=12;
+		int b=8;
+		int c=a-b;
+		System.out.println(c);
+				
+
+	}
+
+}
