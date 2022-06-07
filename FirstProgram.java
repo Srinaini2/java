f00045c39275912080cf94048fd2eafbbc3ca88f
Srinaini2 <107024288+Srinaini2@users.noreply.github.com>
2022-06-07 15:09:22 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="SREE";
+		System.out.println(name);
+
+	}
+
+}
