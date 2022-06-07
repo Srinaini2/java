769e52fe9d48723b6f961e46bc687c6ddfee6c38
Srinaini2 <107024288+Srinaini2@users.noreply.github.com>
2022-06-07 15:42:42 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,26 @@
+package week1.day2;
+
+public class Car {
+	public void driverCar()
+	{
+		System.out.println("Driver car");
+	}
+	public void applyBreak()
+	{
+		System.out.println("ApplyBreak");
+	}
+	public void SwitchOnAc()
+	{
+		System.out.println("SwitchOnAc");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car=new Car();
+		car.driverCar();
+		car.applyBreak();
+		car.SwitchOnAc();
+
+	}
+
+}
