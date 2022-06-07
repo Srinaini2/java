146a9b9f2660df501af64cd281f65ec481697932
Srinaini2 <107024288+Srinaini2@users.noreply.github.com>
2022-06-07 15:51:00 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+package week1.day3;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		int fact=1;
+		//factorial is positive descending integer that is 2!=2*1;denoted by n
+int n=7;
+for(i=1;i<=n;i++)
+{
+	fact=fact*1;
+	System.out.println(fact);
+	
+}
+	}
+
+}
