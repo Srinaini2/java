bdbe7aa067597600d9fbca469cce4c0b1126c7d1
Srinaini2 <107024288+Srinaini2@users.noreply.github.com>
2022-06-07 15:51:43 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,14 @@
+package week1.day3;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
