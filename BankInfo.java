6f76b2d0a5ef3a65be142a6a957d54170ac65da5
Sreenaini <107024288+Srinaini2@users.noreply.github.com>
2022-06-07 19:38:10 +0530

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,17 @@
+package week4.day3;
+
+public class BankInfo {
+
+	public void savings()
+	{
+		System.out.println("SAVING ACCOUNT");
+	}
+	public void fixed()
+	{
+		System.out.println("FIXED ACCOUNT");
+	}
+    public void deposit()
+    {
+    	System.out.println("DEPOSIT");
+    }
+}
